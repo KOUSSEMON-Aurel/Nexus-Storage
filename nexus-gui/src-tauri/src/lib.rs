7ab053e42dfe68e43738feb87af8638a2a2d0424
KOUSSEMON-Aurel <reyseilfullbryger@gmail.com>
2026-04-07 11:05:03 -0500

    fix(linux): remove compositing mode var to prevent white screen and daemon zombie states

--- nexus-gui/src-tauri/src/lib.rs
+++ nexus-gui/src-tauri/src/lib.rs
@@ -15,7 +15,6 @@ pub fn run() {
     unsafe {
         std::env::set_var("GDK_BACKEND", "x11");
         std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
     }
 
     tauri::Builder::default()
